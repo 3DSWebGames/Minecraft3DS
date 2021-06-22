33e7f8594ada7d961b5400cfa38b4fc3fb84c43e
3DSWebGames <86319860+3DSWebGames@users.noreply.github.com>
2021-06-22 15:14:48 -0300

    Add files via upload

---
+++ title.js
@@ -0,0 +1,9 @@
+function load() {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("title").style.display = "block";
+}
+
+function SingleplayerClick() {
+  document.getElementById("title").style.display = "none";
+  document.getElementById("singleplayer").style.display = "block";
+}
